7a09ff968079b7de217b53382057b7a67bafffc3
Maria Grozova <marija.grozova@gmail.com>
2024-08-13 17:58:33 +0000

    add comments in story detail template and admin

--- stories/admin.py
+++ stories/admin.py
@@ -12,4 +12,8 @@ class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
 
 # Register your models here.
-admin.site.register(Comment)
+@admin.register(Comment)
+class CommentAdmin(SummernoteModelAdmin):
+
+    list_display = ('author', 'rating', 'body','approval')
+    list_filter = ('approval',)
--- stories/views.py
+++ stories/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Story
+from .models import Story, Comment
 
 # Create your views here.
 class StoriesList(generic.ListView):
@@ -22,11 +22,17 @@ def story_detail(request, slug):
     :template:`stories/story_detail.html`
     """
 
-    queryset = Story.objects.filter(approval=0)
+    queryset = Story.objects.filter(approval=1)
     story = get_object_or_404(queryset, slug=slug)
+    comments = story.comments.all().order_by("-created_on")
+    comment_count = story.comments.filter(approval=True).count()
 
     return render(
         request,
         "stories/story_detail.html",
-        {"story": story},
+        {
+            "story": story,
+            "comments": comments,
+            "comment_count": comment_count,
+        },
     )

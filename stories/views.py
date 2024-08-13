870323ffc2d8799c229387d86ff59718d7b4d9d9
Maria Grozova <marija.grozova@gmail.com>
2024-08-13 08:46:23 +0000

    added bare bones html templates (base and index extension), set up css file and connected css, google fonts, font awesome to template. added favicon

--- stories/views.py
+++ stories/views.py
@@ -5,3 +5,5 @@ from .models import Story
 # Create your views here.
 class StoriesList(generic.ListView):
     queryset = Story.objects.all()
+    template_name = "stories/index.html"
+    paginate_by = 3
